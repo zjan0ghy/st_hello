e115863fd63df8e05a337a8a8b1a12bea67d69ea
gimmy <123876627+zjan0ghy@users.noreply.github.com>
2023-04-26 15:30:14 +0900

    Add files via upload

---
+++ pages/mpg.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+import pandas as pd
+
+st.set_page_config(
+    page_title="Likelion AI School 자동차 연비 App",
+    page_icon="🚗",
+    layout="wide",
+)
+
+st.markdown("# MPG 🚗")
+st.sidebar.markdown("# MPG 🚗")
+
+st.write("""
+### 자동차 연비
+""")
+
+mpg = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/raw/mpg.csv')
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year',
+   list(reversed(range(mpg.model_year.min(),mpg.model_year.max())))
+   )
+
+# Sidebar - origin
+sorted_unique_origin = sorted(mpg.origin.unique())
+selected_origin = st.sidebar.multiselect('origin', sorted_unique_origin, sorted_unique_origin)
+
+if selected_year > 0 :
+   mpg = mpg[mpg.model_year == selected_year]
+
+if len(selected_origin) > 0:
+   mpg = mpg[mpg.origin.isin(selected_origin)]
+
+st.dataframe(mpg)
+
+st.line_chart(mpg["mpg"])
+
+st.bar_chart(mpg["mpg"])
+
+fig, ax = plt.subplots()
+sns.barplot(data=mpg, x="origin", y="mpg").set_title("origin 별 자동차 연비")
+st.pyplot(fig)
